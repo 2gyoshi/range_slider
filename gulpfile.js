a2d8175058cf8588a3eb0bb222d0d161555a1c65
Yoshifumi-T <2g.4423@gmail.com>
2020-05-17 22:37:21 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const { src, dest, watch } = require("gulp");
+// const compass = require('gulp-compass');
+const sass = require('gulp-sass');
+
+// コンパイル対象のscssファイル
+const conpileTarget = {
+    scss: './src/scss/*.scss'
+};
+
+// コンパイル結果のパス
+const publicPath = {
+    css: './pub/css'
+};
+
+/**
+ * Sassをコンパイルする
+ */
+const compileSass = () =>
+    // style.scssファイルを取得
+    src(`${conpileTarget.scss}`)
+    // Sassのコンパイルを実行
+    // .pipe(
+    //     compass({
+    //         config_file: 'config.rb',
+    //         sass: `${conpileTarget.scss}`,
+    //         css: `${publicPath.css}`
+    //     })
+    // )
+    .pipe(
+        // コンパイル後のCSSを展開
+        sass({
+            outputStyle: "expanded"
+        })
+    )
+    // cssフォルダー以下に保存
+    .pipe(dest(publicPath.css));
+
+/**
+ * Sassファイルを監視し、変更があったらSassを変換する
+ */
+const watchSassFiles = () => watch(conpileTarget.scss, compileSass);
+
+// npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
+exports.default = watchSassFiles;
